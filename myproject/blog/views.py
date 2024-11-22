34ed78f0e7caa169140e54bed393b99ccdf7b157
Evgeny M. Mitushin <mitushinem@gmail.com>
2024-11-22 21:02:58 +0300

    Домашнее задание по теме "Пагинация". 2 часть

--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -1,12 +1,24 @@
+
 from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import BlogPost
 
 def blog_list(request):
     posts = BlogPost.objects.all().order_by('-created_at')
-    paginator = Paginator(posts, 10)  # 10 постов на страницу
+
+    items_per_page = request.GET.get('items_per_page', 10)
+    try:
+        items_per_page = int(items_per_page)
+    except ValueError:
+        items_per_page = 10
+
+    paginator = Paginator(posts, items_per_page)
 
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
-    return render(request, 'blog/blog_list.html', {'page_obj': page_obj})
+    context = {
+        'page_obj': page_obj,
+        'items_per_page': items_per_page,
+    }
+    return render(request, 'blog/blog_list.html', context)
